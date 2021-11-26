c876317ec62b89c6b153a56377aeede69b0b0f5f
Ceri Trammell <cerihaf@gmail.com>
2021-11-26 16:44:50 -0600

    finished GET requests

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const { animals } = require('./data/animals');
 const app = express();
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -112,6 +113,21 @@ function filterByQuery(query, animalsArray) {
     }
   });
   
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+  
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
